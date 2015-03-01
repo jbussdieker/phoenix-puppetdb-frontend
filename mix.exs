de2cc4e825acf6e812a942eb6418c96bb12cf848
Joshua B. Bussdieker <jbussdieker@brightroll.com>
2015-02-28 19:08:50 -0800

    Add some deps for json http apis

--- mix.exs
+++ mix.exs
@@ -15,7 +15,7 @@ defmodule PuppetdbFrontend.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [mod: {PuppetdbFrontend, []},
-     applications: [:phoenix, :cowboy, :logger]]
+     applications: [:phoenix, :cowboy, :logger, :httpotion]]
   end
 
   # Specifies your project dependencies
@@ -23,6 +23,9 @@ defmodule PuppetdbFrontend.Mixfile do
   # Type `mix help deps` for examples and options
   defp deps do
     [{:phoenix, "~> 0.9.0"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.1"},
+     {:httpotion, "~> 2.0.0"},
+     {:json, "~> 0.3.0"}]
   end
 end
